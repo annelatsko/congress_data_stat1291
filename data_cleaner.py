dfc733fb70974d1887ee93dae8257d2aa7581a0e
Anne Latsko <anne.latsko@gmail.com>
2017-03-08 18:40:02 -0500

    data organization

---
+++ data_cleaner.py
@@ -0,0 +1,22 @@
+#Annie Latsko
+#OG datafile: congress-terms.csv
+
+og_65_plus_fn = "dirty_data/OG_percent_of_population_65_older.csv"
+clean_65_plus_fn = "clean_data/percentage_65_plus.csv"
+og_25_to_64_fn = "dirty_data/OG_population_25_to_64.csv"
+clean_65_plus_fn = "clean_data/percentage_25_64.csv"
+
+'''
+goals:
+1. get rid of weird date format
+2. trim down percentage to 2 sig digits
+'''
+#def clean_65_plus(dirty, clean):
+
+'''
+goals:
+1. get rid of weird date format
+2. divide population between 25 and 64 by the total population
+3. restrain significant digits 
+'''
+#def clean_25_64(dirty, clean):
