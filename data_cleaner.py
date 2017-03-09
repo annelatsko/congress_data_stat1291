8dd381c44976eaff730422937280196ae0da59fd
Anne Latsko <anne.latsko@gmail.com>
2017-03-09 17:05:08 -0500

    fixed congress-terms

--- data_cleaner.py
+++ data_cleaner.py
@@ -12,13 +12,14 @@ import sys
 #	GDP OG data goes from 				1929 to 2016
 # 	congress-terms.csv goes from 		1947 to 2013
 #	unemployment OG data goes from 		1947 to 2016
+#	population change OG data goes from 2010 to 1937 (??backwards??)
+#	gdp change OG data goes from 		1930 to 2015
 
  
 #change these to get the years that you want, note the info above
 #implemented so that these are inclusive
 MIN_YEAR = 1960	
-MAX_YEAR = 2013
-
+MAX_YEAR = 2010
 
 og_65_plus = "dirty_data/OG_percent_of_population_65_older.csv"
 cleaned_65_plus = "clean_data/percentage_65_plus.csv"
@@ -40,6 +41,9 @@ cleaned_gdp_change = "clean_data/gdp_change.csv"
 
 og_pop_change = "dirty_data/OG_us_population_change_by_year.csv"
 cleaned_pop_change = "clean_data/us_population_change.csv"
+
+og_congress = "dirty_data/OG_congress-terms.csv"
+cleaned_congress = "clean_data/congress-terms.csv"
 
 '''
 fixes date formatting
@@ -220,12 +224,34 @@ def clean_pop_change(dirty,clean):
 				#cleaned = year + ',' + pop_change + ',' + row[2] + '\n'
 				cleaned_data.insert(0, cleaned)			#inserts at end of the list, which flips dates around
 				#cleaned_data.append(cleaned)
-		cleaned_data.insert(0, h)	#this happens later than usual b/c we need to put it at index 0 of list
+				cleaned_data.insert(0, h)	#this happens later than usual b/c we need to put it at index 0 of list
 	f.close()
 
 	write_back_to_file(clean, cleaned_data)
 
-	
+
+'''
+format: 0congress,1chamber,2bioguide,3firstname,4middlename,5lastname,6suffix,7birthday,8state,9party,10incumbent,11termstart,12age
+goals:
+1. change termstart to just year
+3. keep: 1,8,9,10,11,12
+4. constrain years
+'''
+def clean_congress(dirty,clean):
+	cleaned_data = []
+	with open(dirty, 'rb') as f:
+		reader = csv.reader(f)
+		header = next(reader)
+		h = header[1] + ',' + header[8] + ',' + header[9] + ',' + header[10] + ',' + header[11] + ',' + header[12] + '\n'
+		cleaned_data.append(h)
+		for row in reader:	
+			year = clean_year(row[11])
+			if int(year) <= MAX_YEAR and int(year) >= MIN_YEAR:	
+				cleaned = row[1] + ',' + row[8] + ',' + row[9] + ',' + row[10] + ',' + year + ',' + row[12] + '\n'
+				cleaned_data.append(cleaned)
+	f.close()
+
+	write_back_to_file(clean, cleaned_data)
 
 clean_65_plus(og_65_plus,cleaned_65_plus)
 us_population_dict = clean_us_population(og_us_population,cleaned_us_population)
@@ -234,3 +260,4 @@ clean_gdp(og_gdp,cleaned_gdp)
 clean_unemployment(og_unemployment, cleaned_unemployment)
 clean_gdp_change(og_gdp_change,cleaned_gdp_change)
 clean_pop_change(og_pop_change,cleaned_pop_change)
+clean_congress(og_congress,cleaned_congress)
