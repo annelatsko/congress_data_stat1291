a9d513c377f271af9e9a52bb0db668328fa06501
Annie Latsko <anne.latsko@gmail.com>
2017-03-19 10:12:01 -0400

    super basic R analysis, no graphs

---
+++ congress-analysis.R
@@ -0,0 +1,23 @@
+##Annie Latsko
+##Stats 1291 Group Project 
+##analysis of full_dataset.csv
+
+##constants
+path_to_file <- "C:/Users/Annie/congress_data_stat1291/full_data.csv"    #change this to your own path
+
+
+
+###read in the data
+congress.df <- read.csv(path_to_file)  # read csv file 
+head(congress.df)
+
+
+###Average age of congress per year (basically what fivethirtyeight did)
+require(plyr)
+avg_age_per_year <- ddply(congress.df, .(termstart), summarize,  avg_age=mean(age))
+head(avg_age_per_year)
+
+###Average age of congress per party per year (basically what fivethirtyeight did)
+#####http://stackoverflow.com/questions/21208801/group-by-multiple-columns-in-dplyr-using-string-vector-input
+cols <- c("termstart","party")
+avg_age_per_party_per_year <- ddply(congress.df, cols, summarize, age=mean(age))
