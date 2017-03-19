9aa9cd28a68f52163c649bae7cc08f4397587d03
Annie Latsko <anne.latsko@gmail.com>
2017-03-19 11:08:09 -0400

    reproduced everything relevant 538 did except plots

--- congress-analysis.R
+++ congress-analysis.R
@@ -4,16 +4,19 @@
 
 ##constants
 path_to_file <- "C:/Users/Annie/congress_data_stat1291/full_data.csv"    #change this to your own path
+path_to_new_members_file <- "C:/Users/Annie/congress_data_stat1291/new_members.csv"
 
-
+##environment setup
+install.packages("plyr")
+require(plyr)
 
 ###read in the data
-congress.df <- read.csv(path_to_file)  # read csv file 
+congress.df <- read.csv(path_to_file)  # read csv file
+new.members.df <- read.csv(path_to_new_members_file)
 head(congress.df)
-
+head(new.members.df)
 
 ###Average age of congress per year (basically what fivethirtyeight did)
-require(plyr)
 avg_age_per_year <- ddply(congress.df, .(termstart), summarize,  avg_age=mean(age))
 head(avg_age_per_year)
 
@@ -21,4 +24,14 @@ head(avg_age_per_year)
 cols <- c("termstart","party")
 avg_age_per_party_per_year <- ddply(congress.df, cols, summarize, age=mean(age))
 
-###Average age of new members in congress
+###Average age of new members in congress (fivethirtyeight did this)
+avg_age_new_members <- ddply(new.members.df, .(termstart), summarize,  avg_age=mean(age))
+head(avg_age_new_members)
+
+
+
+
+
+
+
+
